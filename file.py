f460d7d3541d7fbcf4f2648ded18832ef4cd187e
Facundo Chambó <fchambo@despegar.com>
2017-08-31 15:12:14 -0300

    CTF UK

--- file.py
+++ file.py
@@ -3,5 +3,5 @@ def xor(a, b):     # xor two strings of different lengths
     return "".join(["%s" % (ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
  else:
     return "".join(["%s" % (ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
-print xor('30167b0eb4eef511ec82272b4b47a2d71471','1319057cb23c1dcbf616876372617fff8b48')
+print hex(0x30167b0eb4eef511ec82272b4b47a2d71471^0x1319057cb23c1dcbf616876372617fff8b48)
 
